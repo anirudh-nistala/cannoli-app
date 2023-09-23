79dcac44bf244ae34d31d692622ee34beff438cc
AmandaKwok <AmandaKwok28@gmail.com>
2023-09-22 23:25:46 -0400

    added button functionality

--- src/main.js
+++ src/main.js
@@ -2,3 +2,11 @@ import "../style.css";
 
 const app = document.getElementById("app");
 app.innerHTML = `<div>Solving!</div>`;
+
+
+function generateString() {
+    let myString = "Solving all your problems :)";
+    app.innerHTML = `${myString}`;
+}
+const button = document.getElementById("solve-button");
+button.addEventListener("click", generateString);
