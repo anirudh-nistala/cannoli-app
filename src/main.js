42a784c160331f0f14e4706d911980d8dc115a40
AmandaKwok <AmandaKwok28@gmail.com>
2023-09-22 23:46:30 -0400

    added a timer

--- src/main.js
+++ src/main.js
@@ -8,3 +8,30 @@ button.addEventListener("click", () => {
     let myString = "Solving all your problems :)";
     app.innerHTML = `${myString}`;
 });
+
+let timerInterval;
+let seconds = 0;
+
+function startTimer() {
+    clearInterval(timerInterval);
+    seconds = 0;
+    timerInterval = setInterval(updateTimer, 1000); // Update the timer every second (1000 milliseconds)
+
+    // Automatically stop the timer after 60 seconds (1 minute)
+    setTimeout(stopTimer, 60000); // 60,000 milliseconds = 60 seconds
+}
+
+function updateTimer() {
+    seconds++;
+    timer.innerHTML = `Timer: ${seconds} seconds`;
+}
+
+function stopTimer() {
+    clearInterval(timerInterval);
+}
+
+const buttonStart = document.getElementById("start-button");
+buttonStart.addEventListener("click", startTimer);
+
+const buttonStop = document.getElementById("stop-button");
+buttonStop.addEventListener("click", stopTimer);
